07c71d15c77a3749d3f3815950ffa7c3e281b52f
David Silver <dsilver@candidatemetrics.com>
2012-09-07 14:16:05 -0700

    create BasePresenter

--- profile/app/presenters/user_presenter.rb
+++ profile/app/presenters/user_presenter.rb
@@ -1,21 +1,15 @@
-class UserPresenter
-  def initialize(user, template)
-    @user = user
-    @template = template
-  end
+class UserPresenter < BasePresenter
+  presents :user
 
   def avatar
-    h.link_to_if @user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), @user.url
+    h.link_to_if user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), user.url
   end
 
   private
-  def h
-    @template
-  end
 
   def avatar_name
-    if @user.avatar_image_name.present?
-      @user.avatar_image_name
+    if user.avatar_image_name.present?
+      user.avatar_image_name
     else
       "default.png"
     end
